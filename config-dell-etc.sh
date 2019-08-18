1ed356ce3234a2e3a4eef2e852ed20ff5313f7a6
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-08-18 14:06:39 +0200

    remove config files

--- config-dell-etc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export BACKUP_SRC_DIR=/etc/
-export BACKUP_DST_DIR=/mnt/wd-orange/backup/dell/r/etc/
--- config-dell-home.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export BACKUP_SRC_DIR=/home/
-export BACKUP_DST_DIR=/mnt/wd-orange/backup/dell/r/home/
